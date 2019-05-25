acfe079efe3e95463c12e311504e0f959c04d2fe
Nagaraju <vpp.nagaraju1234@gmail.com>
2019-05-25 16:39:55 +0530

    first commit

---
+++ generate_srt.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+#Generate a srt file using lang file and timeline file
+#pip3 install pysrt
+#this will produce four output files (placeholder, new lines, without new lines, story mode)
+
+from argparse import ArgumentParser
+import os
+import sys
+import pysrt
+import re
+
+parser = ArgumentParser(description='Generate srt from lang file and timeline file \n\r'+
+						"How to Run?\n" +
+						"python3 " + sys.argv[0] + " -i=lang.txt " + "-t=timeline.txt"
+						)
+parser.add_argument("-i", "--input", dest="inputfile",
+	help="provide .txt lang file name",required=True)
+parser.add_argument("-t", "--timeline",
+	dest="timeline", help="t=timeline file",required=True)
+#parser.add_argument("-details" "--details",
+#                                       dest="detail", help ="Optional-Value is yes or no, default is no")
+
+args = parser.parse_args()
+
+inputfile = args.inputfile
+timeline = args.timeline
+#generate srt from timeline and lang file
+def generateSRT(i, t):
+
+	langfilename = i
+	timelinefile = t
+
+	langfp = open(langfilename,"r",encoding='utf8')
+	tlfp = open(timelinefile,"r",encoding='utf8')
+	outfp = open("outfile.srt","w")
+
+	langcontent = langfp.readlines()
+	tlcontent = tlfp.readlines()
+	tl_hash = {}
+
+	count = 1
+	for tls in tlcontent:
+		#tl_hash["SUB____" + str(count)] = tls
+		tl_hash[str(count)] = tls
+		count = count + 1
+
+	lang_iterator = 0
+	out = []
+	for l in langcontent:
+		#l = l.strip()
+		#print("1",l)
+		#l = re.sub(r'(\[SUB____\d+\])([\.])$',r'\2\1',l, flags=re.MULTILINE)
+		#l = re.sub(r'(\[SUB(.+)?\])([\.])$',r'\3\1',l, flags=re.MULTILINE)
+		l = re.sub(r'\.\n','. ',l)
+		#l = re.sub(r"\[SUB____(\d+)\]", r"\n[SUB____\1]\n", l, flags=re.MULTILINE)
+		l = re.sub(r"\[SUB(.+?)\]", r"\n[SUB\1]\n", l, flags=re.MULTILINE)
+		#l = l.strip()
+		out.append(l)
+		#print ("2",l)
+	out1 = "\n".join(out)
+	out = out1.split("\n")
+	#print(out)
+	count = 0
+	fout = []
+	fl = 0
+	for o in out:
+		#print(o,fl)
+		#fl = 0
+		fout.append(count + 1)
+		#outfp.write(str(count + 1)+"\n")
+		if(re.search(r'^\[SUB', o)):
+			fl = 0
+			if(count == 0 ):
+				outfp.write(str(count + 1)+"\n")
+			else:
+				outfp.write("\n\n"+str(count + 1)+"\n")
+			outfp.write(tlcontent[count])
+			count = count + 1
+		else:
+			o1 = o.split(" ")
+			al = len(o1)
+			"""
+			if(al > 8 and fl == 0):
+				fl = 1
+				#o = re.sub(r'(.* ){7}',r'\1\n',o)
+				o = ' '.join(o1[:7]) + "\n" + ' '.join(o1[7:])
+				o = re.sub(r' $','',o)
+				outfp.write(o)
+			else:
+				o = re.sub(r' $','',o)"""
+			outfp.write(o)
+			#outfp.write("\n")
+			#fout.append(o)
+
+	#outfp.write(str(fout)+"\n")
+	print("Subtitle generated Successfully!")
+
+
+generateSRT(inputfile,timeline)
